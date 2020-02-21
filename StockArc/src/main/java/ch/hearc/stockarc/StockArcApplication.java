1f5965ad53b46107e7d3696907762db37637277e
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2020-02-21 09:02:56 +0100

    Init project

---
+++ StockArc/src/main/java/ch/hearc/stockarc/StockArcApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.stockarc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockArcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockArcApplication.class, args);
+	}
+
+}
