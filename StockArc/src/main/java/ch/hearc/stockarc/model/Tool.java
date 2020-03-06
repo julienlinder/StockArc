9077256e7f5602ce6bceafad12bcb4c402c12423
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2020-03-06 10:29:54 +0100

    Add tool model

---
+++ StockArc/src/main/java/ch/hearc/stockarc/model/Tool.java
@@ -0,0 +1,50 @@
+package ch.hearc.stockarc.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tool")
+
+public class Tool {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Long id;
+
+	@Column
+	private String name;
+
+	@Column
+	private Integer quantity;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+}
