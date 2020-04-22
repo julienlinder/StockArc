8203e5e4e468138b66560e3546eb634f177eed09
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2020-04-23 01:21:15 +0200

    Fix tests

--- StockArc/src/main/java/ch/hearc/stockarc/repository/RentRepository.java
+++ StockArc/src/main/java/ch/hearc/stockarc/repository/RentRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import ch.hearc.stockarc.model.Rent;
 
+@Repository
 public interface RentRepository extends JpaRepository<Rent, Long> {
 
     List<Rent> findAllByCreatedAt(Date createdAt);
--- StockArc/src/main/java/ch/hearc/stockarc/repository/ToolRepository.java
+++ StockArc/src/main/java/ch/hearc/stockarc/repository/ToolRepository.java
@@ -1,9 +1,11 @@
 package ch.hearc.stockarc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ch.hearc.stockarc.model.Tool;
 
+@Repository
 public interface ToolRepository extends JpaRepository<Tool, Long> {
 
 }
