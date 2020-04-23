4ea80721d60a2ec3c21446072764bf465685c648
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2020-04-23 08:52:24 +0200

    Close #21

--- StockArc/src/main/java/ch/hearc/stockarc/repository/PeopleRepository.java
+++ StockArc/src/main/java/ch/hearc/stockarc/repository/PeopleRepository.java
@@ -1,9 +1,16 @@
 package ch.hearc.stockarc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Sort;
 
 import ch.hearc.stockarc.model.Person;
 
 public interface PeopleRepository extends JpaRepository<Person, Long> {
-    
+
+    List<Person> findByNameIsContaining(String name);
+
+    List<Person> findByNameIsContaining(String name, Sort sort);
+
 }
--- StockArc/src/main/java/ch/hearc/stockarc/repository/ToolRepository.java
+++ StockArc/src/main/java/ch/hearc/stockarc/repository/ToolRepository.java
@@ -1,9 +1,16 @@
 package ch.hearc.stockarc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Sort;
 
 import ch.hearc.stockarc.model.Tool;
 
 public interface ToolRepository extends JpaRepository<Tool, Long> {
+
+    List<Tool> findByNameIsContaining(String name);
+
+    List<Tool> findByNameIsContaining(String name, Sort sort);
 
 }
