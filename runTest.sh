51a513354deea31770dae55a43248165a01c91b3
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2020-04-24 00:27:31 +0200

    Try to fix katalon on jenkins

--- runTest.sh
+++ runTest.sh
@@ -4,21 +4,6 @@ Xvfb :99 &
 export local_addr="127.0.0.1:8080"
 
 /Katalon_Studio_Linux_64-5.7.1/katalon --args -noSplash -runMode=console \
-	-projectPath="StockArcKatalon" -retry=0 -testSuitePath="Test Suites/AuthentificationRegistration" \
-	-executionProfile="default" -browserType="Chrome (headless)" \
-	-Djava.awt.headless
-
-/Katalon_Studio_Linux_64-5.7.1/katalon --args -noSplash -runMode=console \
-	-projectPath="StockArcKatalon" -retry=0 -testSuitePath="Test Suites/Search" \
-	-executionProfile="default" -browserType="Chrome (headless)" \
-	-Djava.awt.headless
-
-/Katalon_Studio_Linux_64-5.7.1/katalon --args -noSplash -runMode=console \
-	-projectPath="StockArcKatalon" -retry=0 -testSuitePath="Test Suites/EditExorcistProfile" \
-	-executionProfile="default" -browserType="Chrome (headless)" \
-	-Djava.awt.headless
-
-/Katalon_Studio_Linux_64-5.7.1/katalon --args -noSplash -runMode=console \
-	-projectPath="StockArcKatalon" -retry=0 -testSuitePath="Test Suites/Search" \
+	-projectPath="StockArcKatalon" -retry=0 -testSuitePath="Test Suites/Test_RegressionTest" \
 	-executionProfile="default" -browserType="Chrome (headless)" \
 	-Djava.awt.headless
