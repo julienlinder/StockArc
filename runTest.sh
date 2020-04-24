f6b8abc7f3ffa819ca63f9d15d5868c719a1809d
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2020-04-24 02:15:37 +0200

    Update runTest.sh

--- runTest.sh
+++ runTest.sh
@@ -5,5 +5,4 @@ export local_addr="127.0.0.1:8080"
 
 /Katalon_Studio_Linux_64-5.7.1/katalon --args -noSplash -runMode=console \
 	-projectPath="StockArcKatalon" -retry=0 -testSuitePath="Test Suites/Test_RegressionTest" \
-	-executionProfile="default" -browserType="Chrome (headless)" \
-	-Djava.awt.headless
+	-executionProfile="default" -browserType="Chrome (headless)"
